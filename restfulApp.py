2da0cea1b52154525028a253d3a5d907d199f862
Francis Ssemwanga <f-ssemwanga>
2023-09-02 10:46:34 +0100

    defined resource and connected the resource to an endpoint

--- restfulApp.py
+++ restfulApp.py
@@ -8,7 +8,32 @@ api = Api(app)
 
 #define the resources
 class Add(Resource):
-  pass
+  #define post and get methods
+  def post(self):
+    #handles resources add requested using method POST
+    #STEP 1: Get posted data
+    postedData =  request.get_json()
+    x = int(postedData['x'])
+    y = int(postedData['y'])
+    #STEP2: perform computation and prepare return data
+    ret = x+y 
+    retMap = {
+      'Message':ret,
+      'Status Code':200
+    }
+    #STEP3: return data
+    return jsonify(retMap)
+    
+  ''' 
+  THESE ARE ONLY HERE TO DEMO HOW HANDLERS FOR OTHER METHODS WOULD BE IMPLEMENTED
+  def get(self):
+    #handles resources add requested using method GET
+    pass
+  def put(self):
+    pass
+  def delete(self):
+    pass
+ '''
 class Subtract(Resource):
   pass
 
@@ -17,6 +42,9 @@ class Multiply(Resource):
 
 class Divide(Resource):
   pass
+
+#Map resource to an end point
+api.add_resource(Add, "/add")
 
 #run the main application
 if __name__ == "__main__":
