4c93c3d21a295ee135494b3fb96e8f276af2de9b
Francis Ssemwanga <f-ssemwanga>
2023-09-02 13:19:41 +0100

    completed api, with code use and all end points connected

--- restfulApp.py
+++ restfulApp.py
@@ -8,41 +8,77 @@ api = Api(app)
 
 #posted data validity checker
 def checkPostedData(PostedData, functionName):
-  if (functionName =="add"):
+  if functionName in("add", "sub", "mult", ):
     if "x" not in PostedData or "y" not in PostedData:
+      return 301
+    else:
+      return 200
+  elif functionName =="div":
+    if "x" not in PostedData or "y" not in PostedData:
+      return 301
+    elif PostedData["y"] ==0:
       return 302
     else:
       return 200
+#generalise the handle post request method
+def handle_post_request(postedData, operation):
+    status_code = checkPostedData(postedData, operation)
+    if status_code != 200:
+        retJson = {
+            'Message': "An Error happened",
+            'Status Code': status_code
+        }
+        return jsonify(retJson)
+    else:
+        x = int(postedData['x'])
+        y = int(postedData['y'])
+        result = None
+
+        if operation == "add":
+            result = x + y
+        elif operation == "sub":
+            result = x - y
+        elif operation == "mult":
+            result = x * y
+        elif operation == "div":
+            result = x / y
+
+        retMap = {
+            'Message': result,
+            'Status Code': 200
+        }
+        return jsonify(retMap)
 
 #define the resources
 class Add(Resource):
   #define post and get methods
   def post(self):
-    #handles resources add requested using method POST
-    #STEP 1a: Get posted data
-    postedData =  request.get_json()
-    #STEP 1b: Verify validity of posted data
-    status_code = checkPostedData(postedData, "add")
-    if status_code !=200:
-      retJson={
-         'Message':"An Error happened",
-        'Status Code':status_code
-      }
-      return jsonify(retJson)
-    else:
-      x = int(postedData['x'])
-      y = int(postedData['y'])
-      #STEP2: perform computation and prepare return data
-      ret = x+y 
-      retMap = {
-        'Message':ret,
-        'Status Code':200
-      }
-      #STEP3: return data
-      return jsonify(retMap)
-    
-  ''' 
-  THESE ARE ONLY HERE TO DEMO HOW HANDLERS FOR OTHER METHODS WOULD BE IMPLEMENTED
+  #   #handles resources add requested using method POST
+  #   #STEP 1a: Get posted data
+  #   postedData =  request.get_json()
+  #   #STEP 1b: Verify validity of posted data
+  #   status_code = checkPostedData(postedData, "add")
+  #   if status_code !=200:
+  #     retJson={
+  #        'Message':"An Error happened",
+  #       'Status Code':status_code
+  #     }
+  #     return jsonify(retJson)
+  #   else:
+  #     x = int(postedData['x'])
+  #     y = int(postedData['y'])
+  #     #STEP2: perform computation and prepare return data
+  #     ret = x+y 
+  #     retMap = {
+  #       'Message':ret,
+  #       'Status Code':200
+  #     }
+  #     #STEP3: return data
+  #     return jsonify(retMap)
+    postedData = request.get_json()
+    return handle_post_request(postedData, 'add')
+  
+  '''THESE ARE ONLY HERE TO DEMO HOW HANDLERS FOR OTHER METHODS WOULD BE IMPLEMENTED
   def get(self):
     #handles resources add requested using method GET
     pass
@@ -52,16 +88,23 @@ class Add(Resource):
     pass
  '''
 class Subtract(Resource):
-  pass
-
+  def post(self):
+    postedData = request.get_json()
+    return handle_post_request(postedData, 'sub')  
 class Multiply(Resource):
-  pass
-
+  def post(self):
+    postedData = request.get_json()
+    return handle_post_request(postedData, 'mult')  
 class Divide(Resource):
-  pass
+  def post(self):
+    postedData = request.get_json()
+    return handle_post_request(postedData, 'div') 
 
 #Map resource to an end point
 api.add_resource(Add, "/add")
+api.add_resource(Subtract,'/sub')
+api.add_resource(Multiply, "/mult")
+api.add_resource(Divide,'/div')
 
 #run the main application
 if __name__ == "__main__":
